f9222d818b932598ea5b782ad8c92f8acb7389d8
Tomek <tmszjaros@gmail.com>
2021-01-13 11:44:18 +0100

    password

--- src/main/java/com/example/Serwiswydarzen/controlles/EventController.java
+++ src/main/java/com/example/Serwiswydarzen/controlles/EventController.java
@@ -1,6 +1,7 @@
 package com.example.Serwiswydarzen.controlles;
 
 
+import com.example.Serwiswydarzen.dtos.NewEventForm;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,6 +11,11 @@ public class EventController {
 
     @GetMapping("/addevent")
     public String addEvent (Model model){
+
+        final NewEventForm newEventForm = new NewEventForm();
+        newEventForm.setEventTitle("Title");
+        newEventForm.setEventBody("New event");
+        model.addAttribute("newEventForm",newEventForm);
 
 
         return "addevent";
--- src/main/java/com/example/Serwiswydarzen/dtos/NewEventForm.java
+++ src/main/java/com/example/Serwiswydarzen/dtos/NewEventForm.java
@@ -1,7 +1,66 @@
 package com.example.Serwiswydarzen.dtos;
 
+import lombok.Data;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.time.LocalDateTime;
+
 public class NewEventForm {
 
-    private String title;
+    @NotNull
+    @NotBlank(message = "Title can not null and can not have white singht.")
+    private String eventTitle;
+    @DateTimeFormat
+    private LocalDateTime eventDateFrom;
+    @Deprecated
+    private LocalDateTime eventDateTo;
+    @NotNull
+    @Size(min = 20)
+    private String eventBody;
 
+    public String getEventTitle() {
+        return eventTitle;
+    }
+
+    public void setEventTitle(String eventTitle) {
+        this.eventTitle = eventTitle;
+    }
+
+    public LocalDateTime getEventDateFrom() {
+        return eventDateFrom;
+    }
+
+    public void setEventDateFrom(LocalDateTime eventDateFrom) {
+        this.eventDateFrom = eventDateFrom;
+    }
+
+    public LocalDateTime getEventDateTo() {
+        return eventDateTo;
+    }
+
+    public void setEventDateTo(LocalDateTime eventDateTo) {
+        this.eventDateTo = eventDateTo;
+    }
+
+    public String getEventBody() {
+        return eventBody;
+    }
+
+    public void setEventBody(String eventBody) {
+        this.eventBody = eventBody;
+    }
+
+
+    @Override
+    public String toString() {
+        return "NewEventForm{" +
+                "eventTitle='" + eventTitle + '\'' +
+                ", eventDateFrom=" + eventDateFrom +
+                ", eventDateTo=" + eventDateTo +
+                ", eventBody='" + eventBody + '\'' +
+                '}';
+    }
 }
